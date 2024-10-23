d5428f4874705ae8dd3b84d6a52735950c6a2760
DAKAMU254 <muchaikamau137@gmail.com>
2024-10-23 09:18:51 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BookList from "./Components/BookList";
+import "./App.css"
+const App = () => {
+  const books = [
+    { id: 1, title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+    { id: 2, title: "1984", author: "George Orwell", year: 1949 },
+    { id: 3, title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+    { id: 4, title: "Moby Dick", author: "Herman Melville", year: 1851 },
+    { id: 5, title: "War and Peace", author: "Leo Tolstoy", year: 1869 },
+  ];
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Book Collection</h1>
+      <BookList books={books} />
     </div>
   );
-}
+};
 
 export default App;
